157e570c269166740587876996a204d088bb3596
Faylixe <felix.voituret@gmail.com>
2016-05-23 15:07:42 +0200

    Finalizes 0.2.1

--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@ from distutils.core import setup
 setup(
     name = 'conkyutil',
     packages = ['conkyutil'],
-    version = '0.2',
+    version = '0.2.1',
     description = 'Simple wrapper for interact with Conky through live script, or generating Conky text file.',
     author = 'Felix Voituret',
     author_email = 'felix.voituret@gmail.com',
     url = 'https://github.com/Faylixe/conkyutil',
-    download_url = 'https://github.com/Faylixe/conkyutil/tarball/0.2',
+    download_url = 'https://github.com/Faylixe/conkyutil/tarball/0.2.1',
     keywords = ['conky', 'wrapper'],
     classifiers = [
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Text Processing :: Conky'
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Code Generators',
+        'Topic :: Utilities'
     ]
 )
